964d9fc1c2ddbda58a5f1a1b09427c517554a6db
Shuvo Podder <shuvo616@gmail.com>
2021-11-01 23:31:22 +0600

    modal with react hook form

--- src/components/Appointment/AppointmentForm/AppointmentForm.js
+++ src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -19,8 +19,8 @@ Modal.setAppElement("#root");
 
 const AppointmentForm = ({ modalIsOpen, closeModal,appointmentOn,date }) => {
 
-  const {register,handleSubmit,formState: { errors },} = useForm();
-  const onSubmit = (data) => console.log(data);
+  const {register,handleSubmit,formState: { errors }} = useForm();
+  const onSubmit = (data) => {console.log(data)};
 
   return (
     <div>
@@ -32,7 +32,8 @@ const AppointmentForm = ({ modalIsOpen, closeModal,appointmentOn,date }) => {
       >
         <h4 className="text-center">{appointmentOn}</h4>
         <h6 className="text-center">Appointment On {date.toDateString()}</h6>
-        <form className="p-3" onSubmit={handleSubmit(onSubmit)}>
+
+        {/* <form className="p-3" onSubmit={handleSubmit(onSubmit)}>
             <div class="row mb-2">
                 <div class="col">
                     <input type="text" class="form-control" placeholder="First name" aria-label="First name"/>
@@ -70,11 +71,11 @@ const AppointmentForm = ({ modalIsOpen, closeModal,appointmentOn,date }) => {
                 <textarea class="form-control" placeholder="Give us a note" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
             <div class="d-grid gap-2">
-                <button class="btn btn-outline-primary" type="button">Submit Appointment</button>
+                <button class="btn btn-outline-primary" type="submit">Submit Appointment</button>
             </div>
-        </form>
+        </form> */}
 
-        {/* <form class="row g-3 needs-validation container" novalidate>
+        {/* <form class="row g-3 needs-validation container" novalidate onSubmit={handleSubmit(onSubmit)}>
             <div class="col-md-4 position-relative">
                 <label for="validationTooltip01" class="form-label">Your Full Name</label>
                 <input type="text" class="form-control" id="validationTooltip01" value="" required/>
@@ -129,6 +130,47 @@ const AppointmentForm = ({ modalIsOpen, closeModal,appointmentOn,date }) => {
                 <button class="btn btn-primary" type="submit">Submit form</button>
             </div>
         </form> */}
+
+    <form className="p-5" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group mb-2">
+            <input type="text" {...register('name', { required: true })}  placeholder="Your Name" className="form-control" />
+            {errors.name && <span className="text-danger">This field is required</span>}
+
+        </div>
+        <div className="form-group mb-2">
+            <input type="text" {...register('phone', { required: true })}  placeholder="Phone Number" className="form-control" />
+            {errors.phone && <span className="text-danger">This field is required</span>}
+        </div>
+        <div className="form-group mb-2">
+            <input type="text" {...register('email', { required: true })}  placeholder="Email" className="form-control" />
+            {errors.email && <span className="text-danger">This field is required</span>}
+        </div>
+        <div className="form-group row mb-2">
+            <div className="col-4">
+
+                <select className="form-control"  {...register('gender', { required: true })} >
+                    <option disabled={true} value="Not set">Select Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Not set">Other</option>
+                </select>
+                {errors.gender && <span className="text-danger">This field is required</span>}
+
+            </div>
+            <div className="col-4">
+                <input {...register('age', { required: true })} className="form-control"  placeholder="Your Age" type="number" />
+                {errors.age && <span className="text-danger">This field is required</span>}
+            </div>
+            <div className="col-4">
+                <input {...register('weight', { required: true })} className="form-control"  placeholder="Weight" type="number" />
+                {errors.weight && <span className="text-danger">This field is required</span>}
+            </div>
+        </div>
+
+        <div className="form-group text-right d-grid gap-2">
+            <button type="submit" className="btn btn-outline-primary">Send</button>
+        </div>
+    </form>
         
       </Modal>
     </div>
