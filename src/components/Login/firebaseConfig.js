2e20d7ddb41c5c32ed7deb036df6033dacbf8d6b
Shuvo Podder <shuvo616@gmail.com>
2021-11-03 00:08:34 +0600

    firbase setup

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbZEwFm6ydGzNa8o8D3UjU3lyGqslR4To",
+    authDomain: "doctor-72d6d.firebaseapp.com",
+    projectId: "doctor-72d6d",
+    storageBucket: "doctor-72d6d.appspot.com",
+    messagingSenderId: "903953446675",
+    appId: "1:903953446675:web:4a45fc8bd70f3d1a64cce6"
+  };
+  export default firebaseConfig
